90fd50ffa15c540643832142f657d21284b68ce7
Matthieu Mota <matthieumota@gmail.com>
2020-11-19 14:55:53 +0100

    Presenter fonctions

---
+++ 07-fonctions/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$toto = 'titi';
+
+// Déclarer une fonction
+function hello($name, $lang = 'fr') {
+    //if ($lang === 'en') {
+    //    return 'Hello '.$name;
+    //}
+    // On peut utiliser la variable $toto dans la fonction
+    global $toto;
+    echo $toto;
+
+    $translations = [
+        'en' => 'Hello',
+        'fr' => 'Bonjour',
+        'es' => 'Hola',
+    ];
+
+    return $translations[$lang].' '.$name;
+}
+
+echo hello('Fiorella'); // J'affiche le retour de la fonction
+echo '<br />';
+echo strtoupper(hello('Matthieu', 'en'));
+echo '<br />';
+echo hello('Jean', 'es');
