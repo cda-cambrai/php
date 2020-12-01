729f1b8c073c82df7e1bb7f364bf65e6d7bf80e2
Matthieu Mota <matthieumota@gmail.com>
2020-12-01 14:57:05 +0100

    Indication base de données

---
+++ 15-webflix/README.md
@@ -0,0 +1,9 @@
+# SQL
+
+Pour recréer la BDD, on ouvre mcd.mwb dans MySQL WorkBench. On choisit Database > Forward Engineer...
+
+On fait suivant... Suivant... Suivant... S'il faut, ne pas oublier de changer la version de MySQL
+dans Edit > Modeling > MySQL
+
+A partir de là, la base de données est créée, on peut s'y connecter via WorkBench et exécuter les fichiers
+SQL pour ajouter les catégories, les films, les acteurs...
--- 15-webflix/sql/02-insert-movie.sql
+++ 15-webflix/sql/02-insert-movie.sql
@@ -10,6 +10,27 @@ SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE TABLE webflix.movie;
 SET FOREIGN_KEY_CHECKS = 1;
 
-INSERT INTO `webflix`.`movie` (`title`, `released_at`, `description`, `duration`, `cover`, `category_id`)
-VALUES ('Le parrain', '1972-01-01', 'Lorem ipsum', 186, null, 1),
-       ('Die Hard', '1988-01-01', 'Lorem ipsum', 124, 'die-hard.jpg', 2);
+INSERT INTO `webflix`.`movie` (`title`, `released_at`, `description`, `duration`, `cover`, `category_id`) VALUES
+('Le Parrain', '1972-01-01', 'Lorem ipsum', 186, 'le-parrain.jpg', 1),
+('Scarface', '1983-01-01', 'Lorem ipsum', 120, 'scarface.jpg', 1),
+('Les Affranchis', '1990-01-01', 'Lorem ipsum', 145, 'les-affranchis.jpg', 1),
+('Heat', '1995-01-01', 'Lorem ipsum', 146, 'heat.jpg', 1),
+('Die Hard', '1988-01-01', 'Lorem ipsum', 124, 'die-hard.jpg', 2),
+('Demolition Man', '1993-01-01', 'Lorem ipsum', 89, 'demolition-man.jpg', 2),
+('Taken', '2008-01-01', 'Lorem ipsum', 96, 'taken.jpg', 2),
+('Deadpool', '2016-01-01', 'Lorem ipsum', 97, 'deadpool.jpg', 2),
+('The Expandables', '2010-01-01', 'Lorem ipsum', 132, 'the-expandables.jpg', 2),
+('Scream', '1996-01-01', 'Lorem ipsum', 78, 'scream.jpg', 3),
+('Vendredi 13', '1980-01-01', 'Lorem ipsum', 97, 'vendredi-13.jpg', 3),
+('Saw', '2005-01-01', 'Lorem ipsum', 102, 'saw.jpg', 3),
+('Scary Movie', '2000-01-01', 'Lorem ipsum', 79, 'scary-movie.jpg', 3),
+('Star Wars IV Un nouvel espoir', '1977-01-01', 'Lorem ipsum', 160, 'star-wars-iv-un-nouvel-espoir.jpg', 4),
+('Alien', '1979-01-01', 'Lorem ipsum', 145, 'alien.jpg', 4),
+('ET', '1982-01-01', 'Lorem ipsum', 95, 'et.jpg', 4),
+('Robocop', '1987-01-01', 'Lorem ipsum', 98, 'robocop.jpg', 4),
+('The Game', '1997-01-01', 'Lorem ipsum', 96, 'the-game.jpg', 5),
+('Sixième Sens', '1999-01-01', 'Lorem ipsum', 120, 'sixieme-sens.jpg', 5),
+('Usual Suspects', '1995-01-01', 'Lorem ipsum', 114, 'usual-suspects.jpg', 5),
+('Fight Club', '1999-01-01', 'Lorem ipsum', 108, 'fight-club.jpg', 5),
+('Inception', '2010-01-01', 'Lorem ipsum', 107, 'inception.jpg', 5),
+('Deadpool 2', '1019-02-19', 'Lorem ipsum', 93, 'deadpool-2.jpg', 2);
